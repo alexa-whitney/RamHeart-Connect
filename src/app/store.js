b4ad57c425499e2c465d39d4f630f6ce8e880216
alexa-whitney <alexawhitney@gmail.com>
2024-02-22 22:08:07 -0700

    Used redux persist to save the story list

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import storyFormReducer from '../features/storyFormSlice';
+import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  storyForm: storyFormReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    storyForm: storyFormReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { store } from './app/store';
+import { store, persistor } from './app/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
-    </Provider>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>,
   </React.StrictMode>
 );
