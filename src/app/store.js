cdeb772a7c269a645569b690b3b82bb29c6bbbb2
alexa-whitney <alexawhitney@gmail.com>
2024-02-24 22:06:10 -0700

    Only stories data persists now

--- src/app/store.js
+++ src/app/store.js
@@ -2,27 +2,25 @@ import { configureStore } from '@reduxjs/toolkit';
 import storyFormReducer from '../features/storyFormSlice';
 import searchConditionReducer from '../features/searchConditionSlice';
 import { combineReducers } from 'redux';
-import { persistReducer, persistStore } from 'redux-persist'; // Import the persistReducer and persistStore functions
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-// Use the persistReducer function to combine the root reducer with the storage configuration
 const persistConfig = {
-	key: 'root',
-	storage,
+  key: 'root',
+  storage: storage,
+  whitelist: ['storyForm'], // Only persist the storyForm slice
 };
 
 const rootReducer = combineReducers({
-	storyForm: storyFormReducer,
-	search: searchConditionReducer,
+  storyForm: storyFormReducer,
+  search: searchConditionReducer, // Search section will not be persisted
 });
 
-// Combine the reducers into a single reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-	reducer: persistedReducer,
-	search: searchConditionReducer,
+  reducer: persistedReducer,
 });
 
-// Export the store and persistor
 export const persistor = persistStore(store);
+
